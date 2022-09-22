0071043c46623a5a70f191c791d3f5c64f941345
@botterinico <botterinico@gmail.com>
2022-09-21 23:44:13 -0300

    Primer commit proyecto base

---
+++ src/components/ListaPersonaje/ComponentePersonaje.vue
@@ -0,0 +1,21 @@
+<script setup>
+    export default
+import personaje from './components/ListaPersonaje/personaje.vue'
+import lista from './components/ListaPersonaje/lista.vue'
+
+
+</script>
+
+<template>
+    <header>
+        <div class="wrapper">
+            <personaje :id="1"></personaje>
+            <lista :personaje="jedi" />
+            <personaje :id="2" />
+        </div>
+    </header>
+
+    <main>
+
+    </main>
+</template>
