b6b7cdcadc265aad9639d6af1fe0ce38973f385c
@botterinico <botterinico@gmail.com>
2022-09-23 15:42:07 -0300

    Agrega componentePersonaje y personaje

--- src/components/ListaPersonaje/lista.vue
+++ src/components/ListaPersonaje/lista.vue
@@ -1,19 +1,26 @@
 <script>
+import Personaje from './personaje.vue';
 export default {
     data() {
-        return {}
+        return {};
     },
-    props:
-    {
+    props: {
         personaje: {
             type: Object
         }
-    }
+    },
+    methods: {
+        obtener() {
+            console.log(Personaje)
+        }
+    },
+    components: { Personaje }
 }
 </script>
 
 <template>
     <ul>
+        <Personaje @hola="obtener"></Personaje>
         <li v-for="(value, key) in personaje">
             {{key}}: {{ value }}
         </li>
