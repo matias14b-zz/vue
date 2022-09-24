f1f5c782910627817b358d6011eca99c6f323916
@botterinico <botterinico@gmail.com>
2022-09-23 23:00:02 -0300

    agrega buscador

---
+++ src/components/buscadorPersonaje/buscadorPersonaje.vue
@@ -0,0 +1,29 @@
+<script>
+import ComponentePersonaje from '../ListaPersonaje/ComponentePersonaje.vue';
+export default {
+    data() {
+        return {
+            id: null,
+            componentePersonaje: [ComponentePersonaje]
+        };
+    },
+    components: { ComponentePersonaje }
+    ,
+    methods: {
+        obtenerId(id) {
+            console.log(id.target.value)
+            this.id = id.target.value
+        },
+        add(e) {
+
+            this.componentePersonaje.push({ type: ComponentePersonaje});
+        }
+    }
+}
+</script>
+<template>
+    <input type="text" @input="obtenerId">
+    <button @click="add">aaaaaaaaaaaaaaaaaaaa</button>
+    <component v-for="element in this.componentePersonaje" :is="element.type" :idPersonaje="this.id" :key="element.id">
+    </component>
+</template>
